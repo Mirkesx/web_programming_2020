b27f138054e565c67243e43096ac3b9bc50b0eb4
Marco Cavalli <cmarcos46@gmail.com>
2020-04-21 19:25:30 +0200

    Update style.css, batman.js, and 2 more files...

---
+++ Esercizi/nananana/assets/js/batman.js
@@ -0,0 +1,63 @@
+var num_na = parseInt(Math.random() * 20 + 1);
+
+///////////////////////////////////////
+
+console.log("SOLUZIONE 1");
+
+let sol1 = "";
+
+for (let i = 0; i < num_na; i++) {
+    sol1 += "Na";
+}
+console.log(sol1 + ", Batman!");
+
+///////////////////////////////////////
+
+console.log("SOLUZIONE 2");
+
+var batorial = (n) => {
+    if (n < 1)
+        return ", Batman!";
+    else
+        return "Na" + batorial(n - 1);
+};
+console.log(batorial(num_na));
+
+///////////////////////////////////////
+
+console.log("SOLUZIONE 3");
+
+var sol3 = [];
+
+for (let i = 0; i < num_na; i++) {
+    sol3.push("Na");
+}
+sol3.push(", Batman!");
+
+console.log(sol3.reduce((sol, na) => (sol + na)));
+
+///////////////////////////////////////
+
+console.log("SOLUZIONE 4");
+
+var sol4 = [];
+
+sol4.push(", Batman!");
+for (let i = 0; i < num_na; i++) {
+    sol4.push("Na");
+}
+
+var true_sol4 = "";
+
+for (let i = 0; i < num_na; i++) {
+    true_sol4 += sol4.pop();
+}
+true_sol4 += sol4.pop();
+
+console.log(true_sol4);
+
+///////////////////////////////////////
+
+console.log("SOLUZIONE 5");
+
+console.log(_.reduce(_.map(_.range(num_na), () => "Na"), (sol, na) => (sol + na)) + ", Batman!");
