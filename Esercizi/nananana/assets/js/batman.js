c81d60a4ce98b1abb5accdde9053520b510d77a8
Marco Cavalli <cmarcos46@gmail.com>
2020-04-23 14:56:59 +0200

    Update batman.js

--- Esercizi/nananana/assets/js/batman.js
+++ Esercizi/nananana/assets/js/batman.js
@@ -1,6 +1,6 @@
 var num_na = parseInt(Math.random() * 20 + 1);
 
-///////////////////////////////////////
+/////////////////////////////////////// Alert Troll
 
 alert("SOLUZIONE 0");
 
@@ -12,7 +12,7 @@ for (let i = 0; i < 3; i++) {
 }
 alert(sol0 + ", Batman!");
 
-///////////////////////////////////////
+/////////////////////////////////////// Iterativa
 
 console.log("SOLUZIONE 1");
 
@@ -23,53 +23,40 @@ for (let i = 0; i < num_na; i++) {
 }
 console.log(sol1 + ", Batman!");
 
-///////////////////////////////////////
+/////////////////////////////////////// Ricorsiva
 
 console.log("SOLUZIONE 2");
 
-var batorial = (n) => {
+var bat_orial = (n) => {
     if (n < 1)
         return ", Batman!";
     else
-        return "Na" + batorial(n - 1);
+        return "Na" + bat_orial(n - 1);
 };
-console.log(batorial(num_na));
+console.log(bat_orial(num_na));
 
-///////////////////////////////////////
+/////////////////////////////////////// Push/Pop
 
 console.log("SOLUZIONE 3");
 
 var sol3 = [];
 
+sol3.push(", Batman!");
 for (let i = 0; i < num_na; i++) {
     sol3.push("Na");
 }
-sol3.push(", Batman!");
 
-console.log(sol3.reduce((sol, na) => (sol + na)));
+var true_sol3 = "";
 
-///////////////////////////////////////
+for (let i = 0; i < num_na; i++) {
+    true_sol3 += sol3.pop();
+}
+true_sol3 += sol3.pop();
+
+console.log(true_sol3);
+
+/////////////////////////////////////// Lodash Map/Reduce
 
 console.log("SOLUZIONE 4");
-
-var sol4 = [];
-
-sol4.push(", Batman!");
-for (let i = 0; i < num_na; i++) {
-    sol4.push("Na");
-}
-
-var true_sol4 = "";
-
-for (let i = 0; i < num_na; i++) {
-    true_sol4 += sol4.pop();
-}
-true_sol4 += sol4.pop();
-
-console.log(true_sol4);
-
-///////////////////////////////////////
-
-console.log("SOLUZIONE 5");
 
 console.log(_.reduce(_.map(_.range(num_na), () => "Na"), (sol, na) => (sol + na)) + ", Batman!");
